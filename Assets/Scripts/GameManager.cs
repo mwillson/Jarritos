5fe0c3c4009b2af59312a22fb7260ab8c3ab4ed5
DESKTOP-QKAVK6G\Mark <mwillsonid@gmail.com>
2018-01-27 15:05:25 -0800

    created a game manager thing that respawns balls

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	GameObject ball;
+	public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+		ball = Instantiate (ballPrefab, new Vector3(0f,0f), transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RespawnBall(){
+		Destroy (ball);
+		ball = Instantiate (ballPrefab, new Vector3(0f,0f), transform.rotation);
+	}
+}
