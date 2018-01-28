7f0a44819ea8e18fa6864e11929becd77416ef41
LAPTOP-3L1AJR00\jhofm <acubicfox@gmail.com>
2018-01-27 22:03:16 -0800

    add dynamic sound triggering

--- Assets/UITriggerSound.cs
+++ Assets/UITriggerSound.cs
@@ -17,25 +17,39 @@ public class UITriggerSound : MonoBehaviour {
 
     public AudioSource curMusicCue;
     public AudioSource nextMusicCue;
+    public AudioSource nextDynamicMusicCue;
+    public float lastDynamicCueTime;
 
     // Use this for initialization
     void Start () {
+        dynamic_intensity = (float)0.0;
+        dynamic_state = null;
+        dynamic_impulse = (float)0.0;
+        lastDynamicCueTime = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (nextMusicCue != null)
-        {
 
+        if (Time.time > lastDynamicCueTime + 4.0) // suppress dynamic step until at least 4 seconds pass
+        {
+            stepDynamicMusic();
+            lastDynamicCueTime = Time.time;
+        }
+
+        if (nextMusicCue != null || nextDynamicMusicCue != null)
+        {
             bool canCue = false;
 
-            if (curMusicCue == null)
-                canCue = true;
-            else if (curMusicCue.isPlaying && nextMusicCue != curMusicCue)
+            if (curMusicCue == null) // bypass cue playing check
             {
-                if (curMusicCue.timeSamples < 22050/8)
+                canCue = true;
+            }
+            else if (curMusicCue.isPlaying)
+            {
+                if (curMusicCue.timeSamples < 22050 / 8)
                 {
-                    curMusicCue.Stop();
+                    // allow the cuechange to take place if the next cue is not the same
                     canCue = true;
                 }
             } else if (!curMusicCue.isPlaying)
@@ -46,13 +60,83 @@ public class UITriggerSound : MonoBehaviour {
 
             if (canCue)
             {
-                nextMusicCue.Play();
-                curMusicCue = nextMusicCue;
-                nextMusicCue = null;
+                if (nextDynamicMusicCue != null)
+                {
+                    nextMusicCue = nextDynamicMusicCue;
+                    nextDynamicMusicCue = null;
+                }
+                if (nextMusicCue != curMusicCue)
+                {
+                    if (curMusicCue != null)
+                        curMusicCue.Stop();
+                    nextMusicCue.Play();
+                    curMusicCue = nextMusicCue;
+                    nextMusicCue = null;
+                }
             }
 
         }
 	}
+
+    public float dynamic_intensity;
+    public string dynamic_state;
+    public float dynamic_impulse;
+
+    public void cueDynamicBegin()
+    {
+        dynamic_state = "load";
+        dynamic_intensity = (float)0.0;
+        dynamic_impulse = (float)0.0;
+        nextDynamicMusicCue = music1; // load
+    }
+    public void cueDynamicWin()
+    {
+        dynamic_state = null;
+        dynamic_intensity = (float)0.0;
+        dynamic_impulse = (float)0.0;
+        nextDynamicMusicCue = music6; // lose
+    }
+    public void cueDynamicLose()
+    {
+        dynamic_state = null;
+        dynamic_intensity = (float)0.0;
+        dynamic_impulse = (float)0.0;
+        nextDynamicMusicCue = music7; // lose
+    }
+    public void dynamicImpulseIntense()
+    {
+        dynamic_impulse += 1f;
+    }
+    public void dynamicImpulseCalm()
+    {
+        dynamic_impulse -= 1f;
+    }
+    public void stepDynamicMusic()
+    {
+        dynamic_intensity += dynamic_impulse;
+        dynamic_impulse = 0.0f;
+        if (dynamic_state != null)
+        {
+            dynamic_intensity = (float)(dynamic_intensity * 0.8);
+            if (dynamic_state == "load")
+            {
+                nextDynamicMusicCue = music1; // loading
+                dynamic_state = "gameplay";
+                dynamic_intensity = 0.0f;
+            }
+            else if (dynamic_state == "gameplay")
+            {
+                if (dynamic_intensity > 1.0)
+                    nextDynamicMusicCue = music5; // intense
+                else if (dynamic_intensity > 0.5)
+                    nextDynamicMusicCue = music4; // challenging
+                else if (dynamic_intensity > -0.5)
+                    nextDynamicMusicCue = music3; // 
+                else if (dynamic_intensity < -0.5)
+                    nextDynamicMusicCue = music2; // relaxed
+            }
+        }
+    }
 
     public void cueSFX()
     {
