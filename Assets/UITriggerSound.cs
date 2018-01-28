79de1c5c9fec8a68de08e846539004528bbabe62
LAPTOP-3L1AJR00\jhofm <acubicfox@gmail.com>
2018-01-27 19:52:24 -0800

    soundboard and production music assets

--- Assets/UITriggerSound.cs
+++ Assets/UITriggerSound.cs
@@ -4,21 +4,90 @@ using UnityEngine;
 
 public class UITriggerSound : MonoBehaviour {
 
-    public AudioSource audioClip;
+    public AudioSource sfx;
+    public AudioSource music1;
+    public AudioSource music2;
+    public AudioSource music3;
+    public AudioSource music4;
+    public AudioSource music5;
+    public AudioSource music6;
+    public AudioSource music7;
+    public AudioSource music8;
 
-	// Use this for initialization
-	void Start () {
-		
+    public AudioSource curMusicCue;
+    public AudioSource nextMusicCue;
+
+    // Use this for initialization
+    void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (nextMusicCue != null)
+        {
+
+            bool canCue = false;
+
+            if (curMusicCue == null)
+                canCue = true;
+            else if (curMusicCue.isPlaying && nextMusicCue != curMusicCue)
+            {
+                if (curMusicCue.timeSamples < 22050/8)
+                {
+                    curMusicCue.Stop();
+                    canCue = true;
+                }
+            } else if (!curMusicCue.isPlaying)
+            {
+                curMusicCue.Stop();
+                canCue = true;
+            }
+
+            if (canCue)
+            {
+                nextMusicCue.Play();
+                curMusicCue = nextMusicCue;
+                nextMusicCue = null;
+            }
+
+        }
 	}
 
-    public void onClick()
+    public void cueSFX()
     {
-        audioClip.Play();    
+        sfx.Play();    
+    }
+    public void cueMusic1()
+    {
+        nextMusicCue = music1;
+    }
+    public void cueMusic2()
+    {
+        nextMusicCue = music2;
+    }
+    public void cueMusic3()
+    {
+        nextMusicCue = music3;
+    }
+    public void cueMusic4()
+    {
+        nextMusicCue = music4;
+    }
+    public void cueMusic5()
+    {
+        nextMusicCue = music5;
+    }
+    public void cueMusic6()
+    {
+        nextMusicCue = music6;
+    }
+    public void cueMusic7()
+    {
+        nextMusicCue = music7;
+    }
+    public void cueMusic8()
+    {
+        nextMusicCue = music8;
     }
 
 }
